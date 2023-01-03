e06dcdb391ab6cf81758bcf9de67d2f647f50489
Viktoriia Zubanenko <vimin.younmin@gmail.com>
2023-01-03 11:55:13 +0200

    added app.js.server.js:created  server layout

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+    res.status(200).json({
+        mess:'It works'
+    });
+});
+
+modele.export = app;
