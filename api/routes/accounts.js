ae9704eee0be93fe07ce55f96cb06c5c48591208
Viktoriia Zubanenko <vimin.younmin@gmail.com>
2023-01-03 14:30:47 +0200

    changed layout of account routing

--- api/routes/accounts.js
+++ api/routes/accounts.js
@@ -13,4 +13,53 @@ router.post('/', (req, res, next)=>{
     });
 });
 
+router.get('/:accountId', (req, res, next)=>{
+    const id = req.params.accountId;
+    if (id === 'sp'){
+        res.status(200).json({
+            message: "/accounts/sp  get request",
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            message: "/accounts/  get request",
+            id: id
+        });
+    }
+    
+});
+
+router.patch('/:accountId', (req, res, next)=>{
+    const id = req.params.accountId;
+    if (id === 'sp'){
+        res.status(200).json({
+            message: "/accounts/sp  patch request",
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            message: "/accounts/  patch request",
+            id: id
+        });
+    }
+    
+});
+
+router.delete('/:accountId', (req, res, next)=>{
+    const id = req.params.accountId;
+    if (id === 'sp'){
+        res.status(200).json({
+            message: "/accounts/sp  delete request",
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            message: "/accounts/  delete request",
+            id: id
+        });
+    }
+    
+});
+
+
 module.exports = router;
