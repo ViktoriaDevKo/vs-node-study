eff1be23a90a7adff71c984f0438b56433c1515a
Viktoriia Zubanenko <vimin.younmin@gmail.com>
2023-01-03 15:39:34 +0200

    ..

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const accountsRoutes = require('./api/routes/accounts');
 const tasksRoutes = require('./api/routes/tasks');
 const regularTasksRoutes = require('./api/routes/regularTasks');
 
+
+app.use(morgan('dev'));
 //middleware
 app.use('/accounts', accountsRoutes);
-app.use('/tasks/all', tasksRoutes);
+app.use('/tasks', tasksRoutes);
 app.use('/tasks/regular', regularTasksRoutes);
 
 
