33f7dfe35145a926e21d41337c411988b8592ace
VikDev <vimin.younmin@gmail.com>
2023-01-04 21:36:14 +0200

    added morgan in app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const accountsRoutes = require('./api/routes/accounts');
 const tasksRoutes = require('./api/routes/tasks');
 const regularTasksRoutes = require('./api/routes/regularTasks');
 
+app.use(morgan('dev'));
+
 //middleware
 app.use('/accounts', accountsRoutes);
 app.use('/tasks/all', tasksRoutes);
 app.use('/tasks/regular', regularTasksRoutes);
 
+app.use();
 
 module.exports = app;
