75b2a54c0a8e28f4e564842304d443a161ff14ff
VikDev <vimin.younmin@gmail.com>
2023-01-04 21:57:03 +0200

    minor cleanups

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req,res,next)=>{
 });
 
 app.use((error, req, res, next)=>{
-    res.status(error.status );
+    res.status(error.status || 500 );
     res.json({
         error:{
             message : error.message
