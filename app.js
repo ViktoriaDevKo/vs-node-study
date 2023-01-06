0f9c8c7c2afb2df55c3aa46ea56a91c3ad7f9c6d
VikDev <vimin.younmin@gmail.com>
2023-01-06 22:15:06 +0200

    CORS mistakes handling through headers

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//handling CORS mistakes
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+    'Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 //middleware
 app.use('/accounts', accountsRoutes);
 app.use('/tasks', tasksRoutes);
@@ -36,5 +50,4 @@ app.use((error, req, res, next)=>{
 });
 
 
-//TODO: CrossOriginResourceSharing errors handling
 module.exports = app;
