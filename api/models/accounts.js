c215456cbdd4382c6c3ac51d21e85da8ed21025a
VikDev <vimin.younmin@gmail.com>
2023-01-10 21:02:45 +0200

    Updated schemas for the accounts and tasks

---
+++ api/models/accounts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const acountSchema = mongoose.Schema({
+    _id_User: mongoose.Types.ObjectId,
+    _id: mongoose.Types.ObjectId,
+    resourceName: String,
+    login: String,
+    password: String,
+    type: String,
+    tags: Array
+});
+
+module.exports = mongoose.model('Account', acountSchema);
