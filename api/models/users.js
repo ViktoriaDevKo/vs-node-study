4269dc342221a6efc48c326d837150c4af929d8d
VikDev <vimin.younmin@gmail.com>
2023-01-23 05:23:32 +0200

    ..

--- api/models/users.js
+++ api/models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    login: String,
+    login: {type: String, required: true},
     salt: String,
     hash : String,
     type: String
